5e3ca97402bf2889a31dce9f889a972eee556808
cloudminion2023 <cloudminion2023@gmail.com>
2024-08-01 16:10:51 +0800

    create ec2 instance

--- main.tf
+++ main.tf
@@ -7,12 +7,27 @@ resource "aws_s3_bucket" "example_bucket" {
   bucket = "example-bucket-cloudminion-github"
 }
 
-resource "aws_s3_bucket" "example_bucket2" {
-  count  = var.check_duplicate ? 1 : 0
-  bucket = "example-bucket-cloudminion-github-second"
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
+  description = "Allow RDP traffic"
+
+  ingress {
+    description = "Allow rdp"
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_s3_bucket" "example_bucket3" {
-  #   count  = var.check_duplicate ? 1 : 0
-  bucket = "example-bucket-cloudminion-github-third"
+resource "aws_instance" "example" {
+  ami           = "ami-07d9456e59793a7d5" # Replace with your desired Windows AMI
+  instance_type = "t2.micro"
+  key_name      = "my-key-pair" # Replace with your key pair name
+
+  security_groups = [aws_security_group.allow_rdp.id]
+
+  tags = {
+    Name = "My Windows Instance"
+  }
 }
